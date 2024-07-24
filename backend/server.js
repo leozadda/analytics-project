c7b2522550c1e85cdeb0386db4d3cd7d0ac724e9
Leo <leo@Leos-MacBook-Pro.local>
2024-07-25 03:30:25 +0700

    I channged the hard coded url for mongodb uri

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ const formAnalytics = require('./routes/formAnalytics');
 const ipAnalytics = require('./routes/ipAnalytics');
 const totalUsers = require('./routes/totalUsers');
 
-const port = process.env.PORT;
-const uri = "mongodb+srv://leozadda:mR74LoVOBifNitlj@useranalytics.pid8es0.mongodb.net/?retryWrites=true&w=majority&appName=UserAnalytics";
+const port = process.env.PORT || 3000;
+const uri = process.env.MONGODB_URI;
 
 // Create the Express app
 const app = express();
@@ -17,7 +17,7 @@ app.use(cors({origin: process.env.FRONTEND_URL}));
 app.use(express.json()); // Parse JSON request bodies
 
 // Connect to MongoDB
-mongoose.connect(uri)
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
     // Start the server after successful MongoDB connection
@@ -34,3 +34,12 @@ app.use('/', formAnalytics);
 app.use('/', ipAnalytics);
 app.use('/', pageAnalytics);
 app.use('/', totalUsers);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// For Vercel serverless functions
+module.exports = app;
