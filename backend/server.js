2fef1067394fcacfd56ec8bdff5093c9fedd808b
Leo <leo@Leos-MacBook-Pro.local>
2024-07-26 02:01:02 +0700

    changed server.js to what i had it before LLM suggests

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,42 @@ const formAnalytics = require('./routes/formAnalytics');
 const ipAnalytics = require('./routes/ipAnalytics');
 const totalUsers = require('./routes/totalUsers');
 
+const port = 5000;
+
+// Create the Express app
+const app = express();
+
+// Set up middleware
+app.use(cors()); // Allow cross-origin requests
+app.use(express.json()); // Parse JSON request bodies
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://leezadda:ISEFBSsv2flNfGw6@analytics-cluster.kufwm7j.mongodb.net/?retryWrites=true&w=majority&appName=analytics-cluster')
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Start the server after successful MongoDB connection
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
+
+// Set up routes
+app.use('/', formAnalytics);
+app.use('/', ipAnalytics);
+app.use('/', pageAnalytics);
+app.use('/', totalUsers);
+
+/*const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const pageAnalytics = require('./routes/pageAnalytics');
+const formAnalytics = require('./routes/formAnalytics');
+const ipAnalytics = require('./routes/ipAnalytics');
+const totalUsers = require('./routes/totalUsers');
+
 // Create the Express app
 const app = express();
 
@@ -57,3 +93,4 @@ app.use((err, req, res, next) => {
 module.exports = app;
 
 console.log('Server setup complete');
+*/
