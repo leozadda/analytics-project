e2d1dd873332f90efdba841a3966c8556916517e
Leo <leo@Leos-MacBook-Pro.local>
2024-07-26 02:16:25 +0700

    just a shitty test

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,21 @@
 const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Vercel!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+/*const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const pageAnalytics = require('./routes/pageAnalytics');
